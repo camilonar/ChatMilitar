d54aa9289d696dc5cb063de87a4361f67b18092c
SINFO <SINFO@192.168.190.199>
2016-05-31 11:37:24 -0500

    Implementaciòn patròn observer

---
+++ src/observer/Observado.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author SINFO
+ */
+public abstract class Observado {
+    
+    private ArrayList<Observador> misObservadores = new ArrayList<>();
+    
+    public void asociarObservador(Observador o){
+        misObservadores.add(o);
+    }
+    
+    public void retirarObservador(Observador o){
+        for (int i = 0; i < misObservadores.size(); i++) {
+            misObservadores.remove(o);
+        }
+    }
+    
+    public void notificar(){
+        for (int i = 0; i < misObservadores.size(); i++) {
+            misObservadores.get(i).actualizar();
+        }
+    }
+    
+}
